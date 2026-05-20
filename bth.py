4324f678c6f140d2c36cdf60330748d30b9b422f
trung2007hs-jpg <trung2007hs@gmail.com>
2026-05-20 20:33:51 +0700

    Add files via upload

---
+++ bth.py
@@ -0,0 +1,18 @@
+continue_pro = "y"
+while continue_pro != "n":
+    num_employees = int(input("Nhập số lượng nhân viên: "))
+    print()  
+    for i in range(num_employees):
+        print(f"Nhân viên {i + 1}")
+        name = input("Tên nhân viên: ")
+        days_worked = int(input("Số ngày đi làm: "))
+        print("Thông tin nhân viên:")
+        print(f"Tên: {name}")
+        print(f"Số ngày đi làm: {days_worked}")
+        if days_worked < 20:
+            print("Cần cải thiện chuyên cần\n")
+        else:
+            print("Nhân viên chuyên cần tốt\n")
+    choice = input("Tiếp tục chương trình? (y/n): ")
+    continue_pro = choice.strip().lower()
+print("Chương trình kết thúc")
